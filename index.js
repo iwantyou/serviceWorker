8c0be78be3b0188750e4b59997fc8e10d9fcc9bd
duzibo <duzb@asiainfo.com>
2021-05-12 17:02:53 +0800

    chore: init

---
+++ index.js
@@ -0,0 +1,19 @@
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", () => {
+    navigator.serviceWorker.register("./serviceWorker.js", { scope: "./" }).then((reg) => {
+      if (reg.waiting) {
+        // 更新  awaiting只有在有新的service worker加载是且有页面还在使用旧的service worker 会处于等待状态
+      }
+      reg.addEventListener("updatefound", () => {
+        // 表示状态发生改变
+        var service;
+        if (reg.installing) {
+          service = reg.installing;
+          service.onstatechange = () => {
+              
+          }
+        }
+      });
+    });
+  });
+}
---
+++ serviceWoker.js
@@ -0,0 +1,29 @@
+const CASH_NAME = "cache_name"
+
+
+self.addEventListener('install', (event) => {
+    // 安装过程下载缓存一些资源
+    event.waitUntil(caches.open(CASH_NAME).then(cache => {
+       return cache.addAll([
+           "./img.png"
+       ])
+    }))
+})
+
+self.addEventListener('active', (event) => {
+    //主要清理一些旧service worker的资源
+    event.waitUntil(caches.keys().then(r => {
+        if(r !== CASH_NAME) caches.delete()
+    }))
+})
+
+// 对于更新service worker  流程为 
+// 给出提示表示站点有重大更新 -》 由操作者进行点击操作 通过postMessage 向servie woker 工作域发送请求
+
+self.addEventListener('message', (e) => {
+    let message = e.data
+    if(message === "skipWaiting"){
+        self.skipWaiting()
+    }
+})
+// 拦截请求
